83fd312e967881712cf73e378a966f323c373eeb
Tarasevich Timofei <timophey01@gmail.com>
2024-12-10 19:19:27 +0700

    fix signer

--- sign.js
+++ sign.js
@@ -1,22 +1,19 @@
 const web3 = require('@solana/web3.js');
-const bs58 = require('bs58');
+const bs58 = require('bs58').default;
 const { readFileSync } = require('fs');
-const { Transaction, StakeProgram, Authorized, Lockup, PublicKey, SendTransactionError } = web3
-
-const { setTimeout } = require('timers/promises');
+"bs58": "^6.0.0"
+const { Transaction } = web3
 const config = JSON.parse(readFileSync('./config.json'));
 
 (async () => {
 
   let connection = new web3.Connection(web3.clusterApiUrl(config.network), );
 
-  let recentBlockhash = await connection.getRecentBlockhash();
-  console.log(recentBlockhash)
   let tx = Transaction.from(Buffer.from(process.argv[2], 'base64'));
 
   for (const pk of config.privateKeys) {
     const signer = web3.Keypair.fromSecretKey(new Uint8Array(bs58.decode(pk)));
-    tx.sign(signer)
+    tx.partialSign(signer)
   }
   console.log(tx.serialize().toString('base64'))
 })();
